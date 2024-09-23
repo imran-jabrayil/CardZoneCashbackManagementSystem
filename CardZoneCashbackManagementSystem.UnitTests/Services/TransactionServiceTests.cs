1ecc6ffec810d6c0673e4fd1014568c517a735f8
Imran Jabrayilov <c.imran2001@yandex.com>
2024-09-24 00:24:29 +0400

    tests updated

--- CardZoneCashbackManagementSystem.UnitTests/Services/TransactionServiceTests.cs
+++ CardZoneCashbackManagementSystem.UnitTests/Services/TransactionServiceTests.cs
@@ -37,7 +37,7 @@ public class TransactionServiceTests
         unitOfWork.Verify(uow => uow.SaveAsync(), Times.Never());
         actualTransactions.Should().BeEquivalentTo(transactions);
     }
-    
+
     [Theory]
     [InlineAutoData]
     public async Task GetTransactionByIdAsync_WhenCalled_IdExists_TransactionReturned(
@@ -53,72 +53,52 @@ public class TransactionServiceTests
         unitOfWork.Setup(uow => uow.TransactionRepository)
             .Returns(transactionRepository.Object)
             .Verifiable();
-    
+
         var sut = new TransactionService(logger.Object, unitOfWork.Object,
             cashbackClient.Object);
-    
+
         var actualTransaction = await sut.GetTransactionByIdAsync(transaction.Id);
-    
+
         transactionRepository.Verify(tr => tr.GetTransactionByIdAsync(transaction.Id), Times.Once());
         unitOfWork.Verify(uow => uow.TransactionRepository, Times.Once());
         unitOfWork.Verify(uow => uow.SaveAsync(), Times.Never());
         actualTransaction.Should().NotBeNull()
             .And.BeEquivalentTo(transaction);
     }
-    
+
     [Theory]
     [InlineAutoData]
     public async Task AddTransactionAsync_WhenCalled_NothingReturned(
         Transaction transaction,
+        Card card,
         Mock<ILogger<TransactionService>> logger,
         Mock<ICashbackClient> cashbackClient,
+        Mock<ICardRepository> cardRepository,
         Mock<ITransactionRepository> transactionRepository,
         Mock<IUnitOfWork> unitOfWork)
     {
         unitOfWork.Setup(uow => uow.TransactionRepository)
             .Returns(transactionRepository.Object)
             .Verifiable();
-        
+        unitOfWork.Setup(uow => uow.CardRepository)
+            .Returns(cardRepository.Object)
+            .Verifiable();
+        cardRepository.Setup(cr => cr.GetCardByIdAsync(transaction.CardId))
+            .ReturnsAsync(card)
+            .Verifiable();
+
         var sut = new TransactionService(logger.Object, unitOfWork.Object,
             cashbackClient.Object);
-    
+
         await sut.AddTransactionAsync(transaction);
-    
+
         transactionRepository.Verify(tr => tr.AddTransactionAsync(transaction), Times.Once());
+        cardRepository.Verify(cr => cr.GetCardByIdAsync(transaction.CardId), Times.Once());
         unitOfWork.Verify(uow => uow.SaveAsync(), Times.Once());
         unitOfWork.Verify(uow => uow.TransactionRepository, Times.Once());
+        unitOfWork.Verify(uow => uow.CardRepository, Times.Once());
     }
-    
-    [Theory]
-    [InlineAutoData(true)]
-    [InlineAutoData(false)]
-    public async Task DeleteTransactionByIdAsync_WhenCalled_SuccessfulRemovalReturned(
-        bool successfullyRemoved,
-        Transaction transaction,
-        Mock<ILogger<TransactionService>> logger,
-        Mock<ICashbackClient> cashbackClient,
-        Mock<ITransactionRepository> transactionRepository,
-        Mock<IUnitOfWork> unitOfWork)
-    {
-        transactionRepository.Setup(tr => tr.DeleteTransactionByIdAsync(transaction.Id))
-            .ReturnsAsync(successfullyRemoved)
-            .Verifiable();
-        unitOfWork.Setup(uow => uow.TransactionRepository)
-            .Returns(transactionRepository.Object)
-            .Verifiable();
-    
-        var sut = new TransactionService(logger.Object, unitOfWork.Object,
-            cashbackClient.Object);
-    
-        var actualRemoval = await sut.DeleteTransactionByIdAsync(transaction.Id);
-    
-        transactionRepository.Verify(tr => tr.DeleteTransactionByIdAsync(transaction.Id),
-            Times.Once());
-        unitOfWork.Verify(uow => uow.TransactionRepository, Times.Once());
-        unitOfWork.Verify(uow => uow.SaveAsync(), Times.Once());
-        actualRemoval.Should().Be(successfullyRemoved);
-    }
-    
+
     [Theory]
     [AutoData]
     public async Task GetTransactionsAsync_WithValidFromDate_ReturnsFilteredTransactions(
@@ -130,27 +110,27 @@ public class TransactionServiceTests
     {
         // Arrange
         var from = transactions.Max(tx => tx.CreatedAt);
-    
+
         transactionRepository.Setup(r => r.GetTransactionsAsync(from))
             .ReturnsAsync(transactions.Where(t => t.CreatedAt >= from).ToList())
             .Verifiable();
         unitOfWork.Setup(uow => uow.TransactionRepository)
             .Returns(transactionRepository.Object)
             .Verifiable();
-    
+
         var sut = new TransactionService(logger.Object, unitOfWork.Object,
             cashbackClient.Object);
-    
+
         // Act
         var result = await sut.GetTransactionsAsync(from);
-    
+
         // Assert
         result.Should().HaveCount(transactions.Count(t => t.CreatedAt >= from));
         transactionRepository.Verify(r => r.GetTransactionsAsync(from), Times.Once());
         unitOfWork.Verify(uow => uow.SaveAsync(), Times.Never());
         unitOfWork.Verify(uow => uow.TransactionRepository, Times.Once());
     }
-    
+
     [Theory]
     [AutoData]
     public async Task GetTransactionsAsync_WithValidFromAndToDate_ReturnsFilteredTransactions(
@@ -163,7 +143,7 @@ public class TransactionServiceTests
         // Arrange
         var from = transactions.Max(tx => tx.CreatedAt);
         var to = from.AddSeconds(1);
-    
+
         // Simulate the behavior of the repository
         transactionRepository.Setup(r => r.GetTransactionsAsync(from, to))
             .ReturnsAsync(transactions.Where(t => t.CreatedAt >= from && t.CreatedAt < to).ToList())
@@ -171,13 +151,13 @@ public class TransactionServiceTests
         unitOfWork.Setup(uow => uow.TransactionRepository)
             .Returns(transactionRepository.Object)
             .Verifiable();
-    
+
         var sut = new TransactionService(logger.Object, unitOfWork.Object,
             cashbackClient.Object);
-    
+
         // Act
         var result = await sut.GetTransactionsAsync(from, to);
-    
+
         // Assert
         result.Should().HaveCount(transactions.Count(t => t.CreatedAt >= from));
         transactionRepository.Verify(r => r.GetTransactionsAsync(from, to), Times.Once());
